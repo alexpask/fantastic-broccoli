01677bb5641f397f488aa05becf9555a0b2097da
awsprog <pask200@gmail.com>
2017-05-23 21:38:19 +0100

    Name optional

--- src/main/java/awsprog/HelloController.java
+++ src/main/java/awsprog/HelloController.java
@@ -1,12 +1,13 @@
 package awsprog;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 class HelloController {
 	@GetMapping("/hello")
-	Hello hello() {
-		return new Hello("Alex");
+	Hello hello(@RequestParam(defaultValue = "Alex")String name) {
+		return new Hello(name);
 	}
 }
